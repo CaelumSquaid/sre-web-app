bd2dbb313aa84fca42267873b9c02f7f740eb2b1
CaelumSquaid <questerkreg.dev@gmail.com>
2023-10-30 02:06:09 +0800

    changed react fragment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import AboutUs from "@/components/AboutUs";
 import ContactUs from "@/components/ContactUs";
 import Gallery from "@/components/Gallery";
+import React from "react";
 
 export default function Home() {
   return (
-    <>
+    <React.Fragment>
       <AboutUs />
       <Gallery />  
       <ContactUs />
-    </>
+    </React.Fragment>
   );
 }
